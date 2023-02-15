e7eada249c16ed9d98d0490d9121fd694704167e
Lasse Edslev <202010638@post.au.dk>
2023-02-15 20:46:36 +0100

    Snapshot: Added map function for mapping parsed contents

--- src/file_handling/igc_parser.rs
+++ src/file_handling/igc_parser.rs
@@ -1,10 +1,29 @@
-fn add_two_numbers(a: u8, b: u8) -> u8 {
-    a+b
+use std::{error, fs::File, io::Read, string::ParseError};
+use std::fmt::Debug;
+use igc::records::Record;
+
+
+fn map_parsed_contents<F,T>(path: &str, function: F) -> Result<Vec<T>, Box<dyn error::Error>>
+    where F: FnOnce(&Vec<Record>) -> Vec<T>
+{
+    let mut file = File::open(path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    let records: Vec<Record> = contents.lines().map(
+        |line| {
+            Record::parse_line(line).unwrap_or_else(|e| panic!("unable to parse line: {line}"))
+        }
+    ).collect();
+    Ok(function(&records))
 }
 
-#[cfg(test)]
-
-#[test]
-fn two_plus_two_is_four() {
-    assert_eq!(add_two_numbers(2, 2), 4)
+pub fn get_altitudes(path: &str) -> Vec<i16> {
+    let f = |records: &Vec<Record>| records.iter().map(|record| {
+        match record {
+            Record::B(BRecord) => Some(BRecord.gps_alt),
+            _ => None
+        }
+    }).collect();
+    let alts: Vec<Option<i16>> = map_parsed_contents(path, f).unwrap();
+    alts.into_iter().flatten().collect()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+use std::fs::File;
+use std::io::Read;
+use std::time;
+
 mod file_handling;
 
 fn main() {
-    println!("Hello, world!");
+    println!("start");
+    let start = time::Instant::now();
+    let alts = file_handling::igc_parser::get_altitudes("examples/example.igc");
+    for a in alts {
+        println!("\tAltitude: {a}");
+    }
+    println!("{} ms since start", start.elapsed().as_millis());
 }
