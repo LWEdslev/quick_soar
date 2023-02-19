c5f6a0a5127ee04983862bbaa404a5e9df894392
Lasse Edslev <202010638@post.au.dk>
2023-02-19 22:05:02 +0100

    Added pilot info parsing (glider type, competition id, timezone)

--- src/file_handling/mod.rs
+++ src/file_handling/mod.rs
@@ -1,2 +1,3 @@
 pub mod igc_parser;
 mod task;
+mod pilot_info;
---
+++ src/file_handling/pilot_info.rs
@@ -0,0 +1,81 @@
+use regex::Regex;
+
+struct PilotInfo{
+    glider_type: String,
+    comp_id: String,
+    time_zone: i8,
+}
+
+impl PilotInfo {
+    fn parse(contents: &str) -> Self {
+        let glider_type = PilotElem::GliderType.get_element(
+            contents.lines().find(|s| s.trim().starts_with("LCU::HPGTYGLIDERTYPE:")).unwrap()
+        );
+        let comp_id = PilotElem::CompetitionId.get_element(
+            contents.lines().find(|s| s.trim().starts_with("LCU::HPCIDCOMPETITIONID:")).unwrap()
+        );
+        let time_zone = PilotElem::TimeZone.get_element(
+            contents.lines().find(|s| s.trim().starts_with("LCU::HPTZNTIMEZONE:")).unwrap()
+        ).parse::<i8>().unwrap();
+
+        Self {
+            glider_type,
+            comp_id,
+            time_zone,
+        }
+    }
+}
+
+enum PilotElem {
+    GliderType,
+    CompetitionId,
+    TimeZone,
+}
+
+impl PilotElem {
+    fn get_element(&self, description: &str) -> String {
+        let start = match self {
+            PilotElem::GliderType => "LCU::HPGTYGLIDERTYPE:",
+            PilotElem::CompetitionId => "LCU::HPCIDCOMPETITIONID:",
+            PilotElem::TimeZone => "LCU::HPTZNTIMEZONE:",
+        };
+
+        let regex = Regex::new(format!("{start}.+").as_str()).unwrap();
+        let m = regex.find(&description.trim()).unwrap();
+
+        (description.trim()[m.start()+start.len() .. m.end()]).to_string()
+    }
+}
+
+#[cfg(test)]
+
+mod tests {
+    use crate::file_handling::igc_parser;
+    use super::*;
+
+    #[test]
+    fn pilot_info_ast_parsing() {
+        let contents = igc_parser::get_contents("examples/ast.igc").unwrap();
+        let pilot_info = PilotInfo::parse(&contents);
+        assert_eq!(pilot_info.comp_id, "KE");
+        assert_eq!(pilot_info.time_zone, 2);
+        assert_eq!(pilot_info.glider_type, "LS 8");
+
+    }
+
+    #[test]
+    fn negative_time_zone() {
+        let contents =
+            "LCU::HPPLTPILOT:Kevin Kjær Andersen
+            LCU::HPGTYGLIDERTYPE:LS 8
+            LCU::HPGIDGLIDERID:OY-XXK
+            LCU::HPCCLCOMPETITIONCLASS:
+            LCU::HPCIDCOMPETITIONID:KE
+            LCU::HPATS:102153
+            LCU::HPELVELEVATION:30
+            LCU::HPTZNTIMEZONE:-2";
+
+        let pilot_info = PilotInfo::parse(&contents);
+        assert_eq!(pilot_info.time_zone, -2);
+    }
+}
